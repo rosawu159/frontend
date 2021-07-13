bf48338c07ddfc8f7df3ceaeed58c60251f14c1e
rosawu159 <u10050070@gmail.com>
2021-07-13 17:22:24 +0800

    add auth

---
+++ src/auth/Login.vue
@@ -0,0 +1,53 @@
+// src/views/Login.vue
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <input type="text" placeholder="Username" v-model="username" />
+    <input type="text" placeholder="Password" v-model="password" />
+    <input type="button" @click="login" value="Login" />
+    <p v-if="msg">{{ msg }}</p>
+    <div>
+      <input type="button" value="signup" @click="signup" />
+    </div>
+  </div>
+
+</template>
+<script>
+import AuthService from '@/services/AuthService.js';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      msg: ''
+    };
+  },
+  methods: {
+    signup() {
+      this.$store.dispatch('sign-up');
+      this.$router.push('/sign-up');
+    },
+    async login() {
+      try {
+        const credentials = {
+          username: this.username,
+          password: this.password
+        };
+        const response = await AuthService.login(credentials);
+        this.msg = response.msg;
+
+        const token = response.token;
+        const user = response.user;
+
+        this.$store.dispatch('login', { token, user });
+
+        this.$router.push('/');
+      } catch (error) {
+        this.msg = error.response.data.msg;
+      }
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import App from './App.vue'
 import Create from './components/AddInvoice.vue'
 import Edit from './components/EditInvoice.vue'
 import Index from './components/InvoiceList.vue'
+import SignUp from "./auth/SignUp.vue";
+import Login from "./auth/Login.vue";
+import Axios from 'axios';
+import store from './store';
+import Home from "./components/Home.vue";
 Vue.use(VueRouter)
 Vue.config.InvoiceionTip = false
+
+// set auth header
+Axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 
 const routes = [
   {
@@ -23,11 +31,28 @@ const routes = [
     path: '/',
     component: Index
   },
+  {
+    name: 'Home',
+    path: '/home',
+    component: Home
+  },
+  {
+    name: 'Signup',
+    path: '/sign-up',
+    component: SignUp
+  },  
+  {
+    name: 'Login',
+    path: '/login',
+    component: Login
+  },
 ];
 
 const router = new VueRouter({ mode: 'history', routes: routes })
 
+
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
